8752f381f350277aaa9cabf37e34f415a19d28d2
Darian Harrison <darianharrison89@gmail.com>
2021-04-29 17:16:48 -0500

    Update http-consuer.py

--- 3.consumer/http-consuer.py
+++ 3.consumer/http-consuer.py
@@ -1,5 +1,19 @@
 import requests
 
-x = requests.get("http://orthanc:orthanc@10.163.168.140:8042/studies")
-print(x.text)
+USER="orthanc"
+PASS="orthanc"
+HOST="10.163.168.140"
+PORT="8042"
+BASE_URL="http://{}:{}@{}:{}".format(USER,PASS,HOST,PORT)
 
+x = requests.get(BASE_URL+"/studies")
+
+y = requests.get(BASE_URL+"/studies/8a8cf898-ca27c490-d0c7058c-929d0581-2bbf104d")
+print(y.content)
+# curl "http://orthanc:orthanc@10.163.168.140:8042/instances/8a8cf898-ca27c490-d0c7058c-929d0581-2bbf104d/file"
+
+# import matplotlib.pyplot as plt
+# import pydicom
+# ds = pydicom.dcmread("sample.dcm")
+# plt.imshow(ds.pixel_array, cmap=plt.cm.bone) 
+# plt.show()
