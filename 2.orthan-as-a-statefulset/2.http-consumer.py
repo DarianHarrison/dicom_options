5b3985413fde90ac91b6accad2d4c2e9f4fca71d
Darian Harrison <darianharrison89@gmail.com>
2021-04-29 17:51:33 -0500

    clean up

--- 2.orthan-as-a-statefulset/2.http-consumer.py
+++ 2.orthan-as-a-statefulset/2.http-consumer.py
@@ -1,8 +1,6 @@
 
 # starter code for consuming dicom files with requests library and orthanc_rest_client
 
-import requests
-
 ##########################
 # CONSTANTS
 ##########################
@@ -11,15 +9,19 @@ USER="orthanc"
 PASS="orthanc"
 HOST="10.163.168.140"
 PORT="8042"
-BASE_URL="http://{}:{}@{}:{}".format(USER,PASS,HOST,PORT)
 
+HARD_CODED_FILENAME="f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af"
 
 ##########################
 # SMOKE TEST
 ##########################
 
+import requests
+BASE_URL="http://{}:{}@{}:{}".format(USER,PASS,HOST,PORT)
 x = requests.get(BASE_URL+"/instances")
 print(x.text)
+
+# HARD_CODED_FILENAME=eval(x.text)[0]
 
 ##########################
 # SAVE TO FILE
@@ -27,8 +29,10 @@ print(x.text)
 from orthanc_rest_client import Orthanc
 from requests.auth import HTTPBasicAuth
 
+REST_BASE_URL='http://{}:{}'.format(HOST,PORT)
+
 auth = HTTPBasicAuth(USER, PASS)
-orthanc = Orthanc('http://{}:{}'.format(HOST,PORT), auth=auth, warn_insecure=False)
+orthanc = Orthanc(REST_BASE_URL, auth=auth, warn_insecure=False)
 
 def save_dcm_file(instance_id):
     fileName = '.'.join([instance_id, "dcm"])
@@ -36,7 +40,7 @@ def save_dcm_file(instance_id):
         for chunk in orthanc.get_instance_file(instance_id):
             dcm.write(chunk)
 
-save_dcm_file("f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af")
+save_dcm_file(HARD_CODED_FILENAME)
 
 
 ##########################
@@ -44,6 +48,6 @@ save_dcm_file("f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af")
 ##########################
 import matplotlib.pyplot as plt
 import pydicom
-ds = pydicom.dcmread("f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af.dcm")
+ds = pydicom.dcmread(HARD_CODED_FILENAME+".dcm")
 plt.imshow(ds.pixel_array, cmap=plt.cm.bone) 
 plt.show()
